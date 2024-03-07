51890764bee677c4dd0faeb01ed10e4a39fa156f
MaximillianoNic <maximilliano@okadoc.com>
2024-03-07 08:47:02 +0700

    feat(auth): added logic auth for internal API

--- app/infrastructure/middleware/guard-route.ts
+++ app/infrastructure/middleware/guard-route.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request } from "express";
+import { NextFunction, Request, Response } from "express";
 
 export interface IRequestMiddleware extends Request {
   userId?: string
@@ -6,6 +6,13 @@ export interface IRequestMiddleware extends Request {
 }
 
 const GuardRoute = (req: IRequestMiddleware, res: Response, next: NextFunction) => {
+  const xToken = req?.get('x-token-api');
+
+  if (xToken !== process.env.API_KEY_INTERNAL) {
+    return res.status(403).send({
+      message: "Forbidden"
+    })
+  }
   const userInfo = req?.get('x-user') ?? '';
   const userData = userInfo?.split(':')
 
